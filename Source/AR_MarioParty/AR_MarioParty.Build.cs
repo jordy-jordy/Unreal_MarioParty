a1a2f6652b3882cb0bedf07295850d3e386fd11c
Jordy_HOME <jordyjordy@naver.com>
2025-02-21 01:30:47 +0900

    이현정 : GameModes 세팅

--- Source/AR_MarioParty/AR_MarioParty.Build.cs
+++ Source/AR_MarioParty/AR_MarioParty.Build.cs
@@ -7,6 +7,8 @@ public class AR_MarioParty : ModuleRules
 	public AR_MarioParty(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.Add(ModuleDirectory);
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
